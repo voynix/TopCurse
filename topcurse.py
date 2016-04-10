d062e0bfc22dbf8dc4498dbdfd32313dd328edee
Adam Jacobson <voynix26@gmail.com>
2016-04-10 10:47:43 -0400

    graphics overhaul; first steps towards iostat integration

--- topcurse.py
+++ topcurse.py
@@ -5,12 +5,19 @@ import time
 
 from collections import deque
 
-TIME_STEP = 5.0 # seconds
-HISTORY_LENGTH = 20
+TIME_STEP = 3.0 # seconds
+HISTORY_LENGTH = 37
+
+SECOND_LEVEL = 12
+MISC_LEVEL = 17
 
 UP_CHAR = '/'
 DOWN_CHAR = '\\'
 FLAT_CHAR = '-'
+
+USER = 'user'
+SYSTEM = 'system'
+IDLE = 'idle'
 
 class ProcessHistory:
     def __init__(self, pid, command):
@@ -57,6 +64,11 @@ swapin_dif = 0
 swapout = 0
 swapout_dif = 0
 
+usage_set = {}
+usage_set[USER] = ProcessHistory(0, USER)
+usage_set[SYSTEM] = ProcessHistory(1, SYSTEM)
+usage_set[IDLE] = ProcessHistory(2, IDLE)
+
 # cleanup curses if something goes horribly wrong
 def quit_curses():
     scr.keypad(0)
@@ -82,9 +94,13 @@ try:
     curses.init_pair(8, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(9, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(10, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(11, curses.COLOR_BLACK, curses.COLOR_WHITE)
     FLAT_CHAR = curses.ACS_HLINE
 
     while True:
+        # clear screen
+        scr.clear()
+
         # gather and extract top data
         top_output = subprocess.check_output("top -n 10 -l 2 -o cpu -stats pid,cpu,command,mem".split(' '))
         lines = top_output.split('\n')[-11:-1]
@@ -98,8 +114,6 @@ try:
             if name not in proc_set:
                 proc_set[name] = ProcessHistory(pid, command)
             proc_set[name].add_sample(current_time, float(cpu), mem)
-
-        scr.clear()
 
         # gather and display vm_stat data
         vmstat_output = subprocess.check_output("vm_stat")
@@ -119,16 +133,21 @@ try:
             elif parts[0] == 'Swapouts:':
                 swapout_dif = value - swapout
                 swapout = value
-        y = 12
+        y = MISC_LEVEL
         x = 0
-        scr.addstr(y, x, "Pageins/out: %i/%i (%i/%i)" % (pagein, pageout, pagein_dif, pageout_dif))
-        scr.addstr(y+1, x, "Swapins/outs: %i/%i (%i/%i)" % (swapin, swapout, swapin_dif, swapout_dif))
+        scr.addstr(y, x, "MEMORY", curses.color_pair(11))
+        scr.addstr(y+1, x, "Pageins/out: %i/%i (+%i/+%i)" % (pagein, pageout, pagein_dif, pageout_dif))
+        scr.addstr(y+2, x, "Swapins/outs: %i/%i (+%i/+%i)" % (swapin, swapout, swapin_dif, swapout_dif))
 
         # sort and display processes
         sorted_list = sorted(cur_list, key=cur_list.get, reverse=True)
         y = 1
-        x = 25
-        scr.addstr(y-1, x, "%5s %-20s %5s %6s" % ("PID", "Process", "CPU", "Memory"))
+        x = 40
+        #scr.addstr(y-1, x+2, "%3s %-20s %5s %6s" % ("PID", "PROCESS", "CPU", "MEMORY"), curses.color_pair(11))
+        scr.addstr(y-1, x+2, "PID", curses.color_pair(11))
+        scr.addstr(y-1, x+6, "PROCESS", curses.color_pair(11))
+        scr.addstr(y-1, x+29, "CPU", curses.color_pair(11))
+        scr.addstr(y-1, x+33, "MEMORY", curses.color_pair(11))
         for i in xrange(0, len(sorted_list)):
             name = sorted_list[i]
             scr.addstr(y, x, "%5s %-20s %5.1f %6s" % (proc_set[name].pid, proc_set[name].command,
@@ -142,7 +161,8 @@ try:
 
         x = 1
         most_recent = len(sorted_lists) - 1
-        base_y = 1
+        y = 1
+        scr.addstr(y-1, x-1, "CPU (RELATIVE USAGE)", curses.color_pair(11))
         for i in xrange(0, len(sorted_lists)):
             if i == 0:
                 if len(sorted_lists) >= HISTORY_LENGTH + 1:
@@ -150,7 +170,7 @@ try:
                 else:
                     for l in xrange(0, len(sorted_lists[most_recent])):
                         if sorted_lists[most_recent][l] in sorted_lists[i]:
-                            scr.addch(base_y + sorted_lists[i].index(sorted_lists[most_recent][l]), x,
+                            scr.addch(y + sorted_lists[i].index(sorted_lists[most_recent][l]), x,
                                       FLAT_CHAR, curses.color_pair(l+1))
             else:
                 for l in xrange(0, len(sorted_lists[most_recent])):
@@ -165,11 +185,12 @@ try:
                                 char = DOWN_CHAR
                             elif diff < 0:
                                 char = UP_CHAR
-                        scr.addch(base_y + sorted_lists[i].index(proc), x, char, curses.color_pair(l+1))
+                        scr.addch(y + sorted_lists[i].index(proc), x, char, curses.color_pair(l+1))
             x += 1
 
-        y = base_y + 11
-        x = 25
+        y = MISC_LEVEL + 2
+        x = 40
+        scr.addstr(y-2, x, "DEBUG", curses.color_pair(11))
         # cull processes that haven't updated recently
         time_history.append(current_time)
         procs_to_kill = []
@@ -179,15 +200,19 @@ try:
                 if proc_set[proc].get_most_recent_time() <= limit_time:
                     #del proc_set[proc]
                     procs_to_kill.append(proc)
-                    scr.addstr(y, x, "culled %s for age" % proc)
+                    scr.addstr(y, x, "Culled %s for age" % proc)
                     y += 1
         for proc in procs_to_kill:
             del proc_set[proc]
 
+        # wait till next iteration
+        delta = TIME_STEP - ((time.time() - start_time) % TIME_STEP)
+        scr.addstr(MISC_LEVEL+1, x, "Sleep Time: %f" % (delta / TIME_STEP))
+
+        # render
         scr.refresh()
 
-        # wait till next iteration
-        time.sleep(TIME_STEP - ((time.time() - start_time) % TIME_STEP))
+        time.sleep(delta)
 except (Exception, KeyboardInterrupt) as e:
     quit_curses()
     print e
