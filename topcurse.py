d54ccaa875c0873b6c9752c3d1b66852ef5d9528
Adam Jacobson <voynix26@gmail.com>
2016-04-10 03:48:42 -0400

    halfway through major revamp, so let's save some work

--- topcurse.py
+++ topcurse.py
@@ -8,6 +8,11 @@ from collections import deque
 TIME_STEP = 5.0 # seconds
 OLD_PROCESS_LIMIT = 20
 
+UP_CHAR = '/'
+DOWN_CHAR = '\\'
+FLAT_CHAR = '-'
+
+# TODO: cull old history items at some point to prevent unbounded memory growth...
 class ProcessHistory:
     def __init__(self, pid, command):
         self.history_times = []
@@ -29,7 +34,8 @@ class ProcessHistory:
 
 start_time = time.time()
 compact_whitespace = re.compile(r'[ \t]+')
-time_history = deque(maxlen=20)
+time_history = deque()
+sorted_lists = deque()
 
 proc_set = {}
 
@@ -46,45 +52,93 @@ try:
     curses.noecho()
     curses.nocbreak()
     scr.keypad(1)
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(6, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(8, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(9, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(10, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     while True:
-            top_output = subprocess.check_output("top -n 10 -l 2 -o cpu -stats pid,cpu,command".split(' '))
-            lines = top_output.split('\n')[-11:-1]
+        # gather and extract data
+        top_output = subprocess.check_output("top -n 10 -l 2 -o cpu -stats pid,cpu,command".split(' '))
+        lines = top_output.split('\n')[-11:-1]
+        current_time = time.time()
+        cur_list = {}
+        for line in lines:
+            parts = compact_whitespace.sub(' ', line).split(' ')
+            pid, cpu, command = parts[:3]
+            name = "%s|%s" % (pid, command)
+            cur_list[name] = float(cpu)
+            if name not in proc_set:
+                proc_set[name] = ProcessHistory(pid, command)
+            proc_set[name].add_sample(current_time, float(cpu))
 
-            current_time = time.time()
-            cur_list = {}
-            for line in lines:
-                parts = compact_whitespace.sub(' ', line).split(' ')
-                pid, cpu, command = parts[:3]
-                name = "%s|%s" % (pid, command)
-                cur_list[name] = float(cpu)
-                if name not in proc_set:
-                    proc_set[name] = ProcessHistory(pid, command)
-                proc_set[name].add_sample(current_time, float(cpu))
+        # sort and display processes
+        sorted_list = sorted(cur_list, key=cur_list.get, reverse=True)
+        y = 0
+        x = 0
+        scr.clear()
+        for name in sorted_list:
+            #print name, proc_set[name].history[current_time]
+            scr.addstr(y, x, "%-16s %5.1f" % (proc_set[name].command, proc_set[name].get_most_recent_cpu()))
+            y += 1
 
-            sorted_list = sorted(cur_list, key=cur_list.get, reverse=True)
-            y = 0
-            x = 0
-            scr.clear()
-            for name in sorted_list:
-                #print name, proc_set[name].history[current_time]
-                scr.addstr(y, x, "%-16s %5.1f" % (proc_set[name].command, proc_set[name].get_most_recent_cpu()))
-                y += 1
+        sorted_lists.append(sorted_list)
+        if len(sorted_lists) > OLD_PROCESS_LIMIT + 1:
+            sorted_lists.popleft()
 
-            # cull processes that haven't updated recently
-            time_history.append(current_time)
-            if len(time_history) > OLD_PROCESS_LIMIT:
-                limit_time = time_history.popleft()
-                for proc in proc_set:
-                    if proc_set[proc].get_most_recent_time() <= limit_time:
-                        del proc_set[proc]
-                        scr.addstr(y, x, "culled %s for age" % proc)
-                        y += 1
+        most_recent = len(sorted_lists) - 1
+        base_y = 12
+        for i in xrange(0, len(sorted_lists)):
+            y = 12
+            if i == 0:
+                if len(sorted_lists) >= OLD_PROCESS_LIMIT + 1:
+                    continue
+                else:
+                    for l in xrange(0, len(sorted_lists[most_recent])):
+                        if sorted_lists[most_recent][l] in sorted_lists[i]:
+                            scr.addch(y, x, FLAT_CHAR, curses.color_pair(l+1))
+                            #y += 1
+            else:
+                for l in xrange(0, len(sorted_lists[i])):
+                    proc = sorted_lists[i][l]
+                    char = FLAT_CHAR
+                    if proc not in sorted_lists[i-1]:
+                        char = UP_CHAR
+                    else:
+                        diff = l - sorted_lists[i-1].index(proc)
+                        if diff > 0:
+                            char = UP_CHAR
+                        elif diff < 0:
+                            char = DOWN_CHAR
+                    scr.addch(y, x, char, curses.color_pair(l+1))
+                    y += 1
+            x += 1
 
-            scr.refresh()
+        # cull processes that haven't updated recently
+        time_history.append(current_time)
+        procs_to_kill = []
+        if len(time_history) > OLD_PROCESS_LIMIT:
+            limit_time = time_history.popleft()
+            for proc in proc_set:
+                if proc_set[proc].get_most_recent_time() <= limit_time:
+                    #del proc_set[proc]
+                    procs_to_kill.append(proc)
+                    scr.addstr(y, x, "culled %s for age" % proc)
+                    y += 1
+        for proc in procs_to_kill:
+            del proc_set[proc]
 
-            # wait till next iteration
-            time.sleep(TIME_STEP - ((time.time() - start_time) % TIME_STEP))
+        scr.refresh()
+
+        # wait till next iteration
+        time.sleep(TIME_STEP - ((time.time() - start_time) % TIME_STEP))
 except (Exception, KeyboardInterrupt) as e:
     quit_curses()
     print e
