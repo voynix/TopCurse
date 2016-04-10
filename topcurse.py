2aad683642af9b431b6f268569fffa7d869e19e2
Adam Jacobson <voynix26@gmail.com>
2016-04-10 03:04:16 -0400

    cleanup process culling, fix process display

--- topcurse.py
+++ topcurse.py
@@ -3,27 +3,33 @@ import re
 import subprocess
 import time
 
-from Queue import Queue
+from collections import deque
 
 TIME_STEP = 5.0 # seconds
 OLD_PROCESS_LIMIT = 20
 
 class ProcessHistory:
     def __init__(self, pid, command):
-        self.history = {}
+        self.history_times = []
+        self.history_cpus = []
         self.pid = pid
         self.command = command
+        self.len = -1
 
     def add_sample(self, time, cpu):
-        self.history[time] = cpu
+        self.history_times.append(time)
+        self.history_cpus.append(cpu)
+        self.len += 1
 
-    def get_most_recent_sample(self):
-        return max(self.history.keys)
+    def get_most_recent_time(self):
+        return self.history_times[self.len]
+
+    def get_most_recent_cpu(self):
+        return self.history_cpus[self.len]
 
 start_time = time.time()
 compact_whitespace = re.compile(r'[ \t]+')
-time_history = Queue(maxsize=20)
-time_history.put(start_time)
+time_history = deque(maxlen=20)
 
 proc_set = {}
 
@@ -54,7 +60,7 @@ try:
                 cur_list[name] = float(cpu)
                 if name not in proc_set:
                     proc_set[name] = ProcessHistory(pid, command)
-                proc_set[name].add_sample(current_time, cpu)
+                proc_set[name].add_sample(current_time, float(cpu))
 
             sorted_list = sorted(cur_list, key=cur_list.get, reverse=True)
             y = 0
@@ -62,19 +68,23 @@ try:
             scr.clear()
             for name in sorted_list:
                 #print name, proc_set[name].history[current_time]
-                scr.addstr(y, x, "%s %s" % (name, proc_set[name].history[current_time]))
+                scr.addstr(y, x, "%-16s %5.1f" % (proc_set[name].command, proc_set[name].get_most_recent_cpu()))
                 y += 1
-            scr.refresh()
 
             # cull processes that haven't updated recently
-            limit_time = time_history.get()
-            time_history.put(current_time)
-            if time_history.full():
+            time_history.append(current_time)
+            if len(time_history) > OLD_PROCESS_LIMIT:
+                limit_time = time_history.popleft()
                 for proc in proc_set:
-                    if proc_set[proc].get_most_recent_sample() <= limit_time:
+                    if proc_set[proc].get_most_recent_time() <= limit_time:
                         del proc_set[proc]
+                        scr.addstr(y, x, "culled %s for age" % proc)
+                        y += 1
+
+            scr.refresh()
 
             # wait till next iteration
             time.sleep(TIME_STEP - ((time.time() - start_time) % TIME_STEP))
-except:
+except (Exception, KeyboardInterrupt) as e:
     quit_curses()
+    print e
