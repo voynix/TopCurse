efe136aa09801bff95eca58f3663d830f7dc76fd
Adam Jacobson <voynix26@gmail.com>
2016-04-10 05:44:27 -0400

    added memory listing

--- topcurse.py
+++ topcurse.py
@@ -16,17 +16,20 @@ class ProcessHistory:
     def __init__(self, pid, command):
         self.history_times = deque()
         self.history_cpus = deque()
+        self.history_mems = deque()
         self.pid = pid
         self.command = command
         self.len = -1
 
-    def add_sample(self, time, cpu):
+    def add_sample(self, time, cpu, mem):
         self.history_times.append(time)
         self.history_cpus.append(cpu)
+        self.history_mems.append(mem)
         self.len += 1
         if self.len > HISTORY_LENGTH:
             self.history_times.popleft()
             self.history_cpus.popleft()
+            self.history_mems.popleft()
             self.len -= 1
 
     def get_most_recent_time(self):
@@ -34,6 +37,9 @@ class ProcessHistory:
 
     def get_most_recent_cpu(self):
         return self.history_cpus[self.len]
+
+    def get_most_recent_mem(self):
+        return self.history_mems[self.len]
 
 start_time = time.time()
 compact_whitespace = re.compile(r'[ \t]+')
@@ -70,28 +76,30 @@ try:
 
     while True:
         # gather and extract data
-        top_output = subprocess.check_output("top -n 10 -l 2 -o cpu -stats pid,cpu,command".split(' '))
+        top_output = subprocess.check_output("top -n 10 -l 2 -o cpu -stats pid,cpu,command,mem".split(' '))
         lines = top_output.split('\n')[-11:-1]
         current_time = time.time()
         cur_list = {}
         for line in lines:
             parts = compact_whitespace.sub(' ', line).split(' ')
-            pid, cpu, command = parts[:3]
+            pid, cpu, command, mem = parts[:4]
             name = "%s|%s" % (pid, command)
             cur_list[name] = float(cpu)
             if name not in proc_set:
                 proc_set[name] = ProcessHistory(pid, command)
-            proc_set[name].add_sample(current_time, float(cpu))
+            proc_set[name].add_sample(current_time, float(cpu), mem)
 
         # sort and display processes
         sorted_list = sorted(cur_list, key=cur_list.get, reverse=True)
         y = 1
         x = 25
         scr.clear()
-        scr.addstr(y-1, x, "%5s %-20s %5s" % ("PID", "Process", "CPU"))
+        scr.addstr(y-1, x, "%5s %-20s %5s %6s" % ("PID", "Process", "CPU", "Memory"))
         for i in xrange(0, len(sorted_list)):
             name = sorted_list[i]
-            scr.addstr(y, x, "%5s %-20s %5.1f" % (proc_set[name].pid, proc_set[name].command, proc_set[name].get_most_recent_cpu(), ), curses.color_pair(i+1))
+            scr.addstr(y, x, "%5s %-20s %5.1f %6s" % (proc_set[name].pid, proc_set[name].command,
+                                                      proc_set[name].get_most_recent_cpu(),
+                                                      proc_set[name].get_most_recent_mem()), curses.color_pair(i+1))
             y += 1
 
         sorted_lists.append(sorted_list)
@@ -108,7 +116,8 @@ try:
                 else:
                     for l in xrange(0, len(sorted_lists[most_recent])):
                         if sorted_lists[most_recent][l] in sorted_lists[i]:
-                            scr.addch(base_y + sorted_lists[i].index(sorted_lists[most_recent][l]), x, FLAT_CHAR, curses.color_pair(l+1))
+                            scr.addch(base_y + sorted_lists[i].index(sorted_lists[most_recent][l]), x,
+                                      FLAT_CHAR, curses.color_pair(l+1))
             else:
                 for l in xrange(0, len(sorted_lists[most_recent])):
                     proc = sorted_lists[most_recent][l]
