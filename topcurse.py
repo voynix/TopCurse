8107eb6cb25ad3e128cd6ae177b1e04fb26a4573
Adam Jacobson <voynix26@gmail.com>
2016-04-10 05:44:59 -0400

    make cursor invisible

--- topcurse.py
+++ topcurse.py
@@ -61,6 +61,7 @@ try:
     curses.noecho()
     curses.nocbreak()
     scr.keypad(1)
+    curses.curs_set(0)
     curses.start_color()
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
